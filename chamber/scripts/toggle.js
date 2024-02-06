32ed980aec729d93c301469305f1aa33b82bb4c8
heavenlyaura <goodnessokafor462@gmail.com>
2024-02-07 00:56:21 +0100

    05: Chamber of Commerce Home Page Continued

---
+++ chamber/scripts/toggle.js
@@ -0,0 +1,8 @@
+const modeToggle = document.querySelector('#mytoggle');
+const main = document.querySelector('main');
+const nav = document.querySelector('nav');
+
+modeToggle.addEventListener('change', ()=> {
+    main.classList.toggle('darkmode')
+    nav.classList.toggle('navigation-dark')
+})
