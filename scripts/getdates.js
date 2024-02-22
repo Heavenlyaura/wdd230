dd44186e180826d5e3a8c5768ab7e392f3d488ab
heavenlyaura <goodnessokafor462@gmail.com>
2024-02-22 17:27:44 +0100

    submission

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,20 @@
 // copyright
 const copyrightSymbol = '\u00A9';
 const copyright = document.getElementById("copyright").innerText;
+const visits = document.querySelector('.visit');
+let numVisits = Number(localStorage.getItem('numVisit') || 0)
+
+
+if (numVisits !== 0) {
+    visits.textContent = `Visited: ${numVisits}`;
+}
+else {
+    visits.textContent = 'This is your first visit! Welcome!!'
+}
+
+numVisits++;
+
+localStorage.setItem('numVisit', numVisits)
 
 document.getElementById('copyright').innerText = `Goodness A. Okafor ${copyrightSymbol} 2024 ${copyright}`;
 
