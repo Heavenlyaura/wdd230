f98b45a00b35c11c97066ab04fc99f4c2ddeced9
heavenlyaura <goodnessokafor462@gmail.com>
2024-04-01 19:31:56 +0100

    submit page 2

--- final-project/scripts/getrentals.js
+++ final-project/scripts/getrentals.js
@@ -1,8 +1,12 @@
 const url = "https://raw.githubusercontent.com/Heavenlyaura/wdd230/main/final-project/data/rentals.json"
 const table = document.querySelector('table');
-// const reservationHead = document.querySelector('.reserve');
-// const walkInHead = document.querySelector('.walk-in');
-
+const tBody = document.querySelector('.tbody');
+const typeHead = document.querySelector('.type')
+const personsHead = document.querySelector('.persons')
+const reservationHead = document.querySelector('.reserve');
+const walkInHead = document.querySelector('.walk-in');
+const walkinHead = document.querySelector('.walk-in')
+const reserveHead = document.querySelector('.reserve')
 
 
 
@@ -24,8 +28,21 @@ function displayRentals(data) {
   rentalsData.forEach(data => {
     let type = data.Type;
     let maxPersons = data.MaxPersons;
-    let reserveFull = data.Reservations;
+    let reserveFull = data.Reservations.fullDay;
+    let reserveHalf = data.Reservations.halfDay;
+    let walkFull = data.Walkin.halfDay;
+    let walkHalf = data.Walkin.fullDay;
 
+    let row = `<tr> 
+    <td data-cell="name" >${type}</td>
+    <td data-cell="maxpersons" >${maxPersons}</td>
+    <td data-cell="reservefull" >${reserveFull}</td>
+    <td data-cell="reserveHalf" >${reserveHalf}</td>
+    <td data-cell="walkFull" >${walkFull}</td>
+    <td data-cell="walkHalf" >${walkHalf}</td>
+    </tr>`
+          
+        tBody.innerHTML += row
   });
 
 }
